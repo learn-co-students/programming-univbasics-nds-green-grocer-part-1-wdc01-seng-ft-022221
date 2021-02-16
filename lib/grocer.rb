2ff0f0ce144d3b2bb786f5d7198b202543649497
jacjyd <6047796+jacjyd@users.noreply.github.com>
2021-02-15 23:27:04 -0500

    Update grocer.rb

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,9 +1,6 @@
 require 'pry'
 
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
   
   result = nil
   
@@ -14,18 +11,14 @@ def find_item_by_name_in_collection(name, collection)
   end 
   
   return result
-  
+ 
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
   simple_cart = []
-  
-  
   
   cart.each do |listing|
     
@@ -39,9 +32,7 @@ def consolidate_cart(cart)
          e[:count] += 1
       end 
     end
-    
   end
-
   
   simple_cart
 
